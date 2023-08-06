6fb304109dccca0cfe133cd9aa46229a92ed8b7d
Juan Pablo Enrici <jpenrici@gmail.com>
2023-08-06 18:53:36 -0300

    Under development ...

--- SVG_SimpleToolBox/src/svgToolBox.h
+++ SVG_SimpleToolBox/src/svgToolBox.h
@@ -2,12 +2,21 @@
 #define _SMALLTOOLBOX_H_
 
 #include <array>
+#include <ctime>
+#include <fstream>
 #include <iostream>
 #include <math.h>
+#include <random>
 #include <string>
 #include <vector>
 
 namespace smalltoolbox {
+
+constexpr auto WHITE = "#FFFFFF";
+constexpr auto BLACK = "#000000";
+constexpr auto RED   = "#FF0000";
+constexpr auto GREEN = "#00FF00";
+constexpr auto BLUE  = "#0000FF";
 
 // (x,y)
 class Point {
@@ -35,12 +44,14 @@ class Point {
 
 public:
 
+    // Axis
     Coordinate X, Y;
 
-    // Point(0, 0)
+    // Point (0, 0)
     Point() : X{0}, Y{0} {};
-    // Point(x, y)
+    // Point (x, y)
     Point(double x, double y) : X{x}, Y{y} {};
+
     ~Point() {};
 
     Point operator+(Point point);
@@ -72,42 +83,120 @@ public:
 
 };
 
-class Polygon {
-
-    Point center;
-    unsigned sides;
-
-    std::vector<Point> points;
-
-public:
-
-    Polygon(Point center, unsigned sides);
-    ~Polygon() {};
-};
-
 class Line {
 
 public:
 
     Point first, second;
 
-    Line() : first(Point()), second(Point()) {};
+    // Line : Origin (0,0) to Point (x,y)
+    Line(Point point) : first(Point()), second(point) {};
+    // Line : Point 1 (x1,y1) to Point 2 (x,y)
     Line(Point first, Point second) : first(first), second(second) {};
+
     ~Line() {};
 
     double length();
+    std::vector<Point> points();
 };
 
+class Polygon {
+
+    Point center;
+    double angle;
+    double radius;
+    unsigned sides;
+    std::vector<Point> vertices;
+
+public:
+
+    Polygon() {};
+    ~Polygon() {};
+
+    // Center : polygon center point (x,y),
+    // radius : distance from the center (>= 1),
+    // angle  : starting point angle,
+    // sides  : divisions of a circle (>= 3).
+    std::vector<Point> setup(Point center, double radius, double angle, unsigned sides);
+
+    // Return last vertices.
+    std::vector<Point> points();
+
+    double sideLength();
+    double diagonalLength();
+
+};
+
+// SVG
+class SVG {
+
+public:
+
+    struct Metadata {
+        std::string creator = "SVG created automatically by algorithm in C++.";
+        std::string title = "SVG";
+        std::string publisherAgentTitle = "";
+        std::string date = "";
+
+        Metadata() {};
+        Metadata(std::string creator, std::string title, std::string publisher)
+            : creator(creator), title(title), publisherAgentTitle(publisher) {}
+    };
+
+    struct Shape {
+        std::string name, fill, stroke;
+        double strokeWidth;
+        double fillOpacity, strokeOpacity; // 0.0 = 0 = 0%; 255 = 1.0 = 100%
+        std::vector<Point> points;
+
+        Shape()
+            : name("Shape"), fill(WHITE), stroke(BLACK), strokeWidth(1.0),
+              fillOpacity(255.0), strokeOpacity(255.0), points({}) {}
+
+        Shape(std::string name,  std::string fill, std::string stroke, double strokeWidth)
+            : name(name), fill(fill), stroke(stroke), strokeWidth(strokeWidth),
+              fillOpacity(255.0), strokeOpacity(255.0), points({}) {}
+        Shape(std::string name,  std::string fill, std::string stroke, double strokeWidth,
+              std::vector<Point> points)
+            : name(name), fill(fill), stroke(stroke), strokeWidth(strokeWidth),
+              fillOpacity(255.0), strokeOpacity(255.0), points(points) {}
+
+        Shape(std::string name,  std::string fill, std::string stroke, double strokeWidth,
+              double fillOpacity, double strokeOpacity)
+            : name(name), fill(fill), stroke(stroke), strokeWidth(strokeWidth),
+              fillOpacity(fillOpacity), strokeOpacity(strokeOpacity), points({}) {}
+        Shape(std::string name,  std::string fill, std::string stroke, double strokeWidth,
+              double fillOpacity, double strokeOpacity, std::vector<Point> points)
+            : name(name), fill(fill), stroke(stroke), strokeWidth(strokeWidth),
+              fillOpacity(fillOpacity), strokeOpacity(strokeOpacity), points(points) {}
+    };
+
+    static const std::string INT2HEX(unsigned value);
+    static const std::string RGB2HEX(unsigned R, unsigned G, unsigned B);
+    static const std::string RGBA2HEX(unsigned R, unsigned G, unsigned B, unsigned A);
+
+    static const std::string group(std::string id, std::string elements);
+    static const std::string polyline(Shape shape);
+    static const std::string polygon(Shape shape);
+    static const std::string svg(int width, int height, const std::string &xml,
+                                 Metadata metadata);
+};
+
+// Generic
 double radians(double angle);
 double angle(double radians);
 double angle(double x0, double y0, double x1, double y1);
-double cos(double value, double radius, int angle);
-double sin(double value, double radius, int angle);
+double cos(double value, double radius, double angle);
+double sin(double value, double radius, double angle);
 double distance(double x0, double y0, double x1, double y1);
 
 void view(double value);
 void view(Point point);
 void view(std::array<double, 2> arr);
+void view(std::vector<Point> points);
+void view(std::string str);
+
+bool save(const std::string &text, std::string path = "");
 
 } // namespace
 
--- SVG_SimpleToolBox/src/test.cpp
+++ SVG_SimpleToolBox/src/test.cpp
@@ -9,24 +9,94 @@ void test();
 int main()
 {
     test();
+
     return 0;
 }
 
 void test()
 {
+    // Generic
+    assert(smalltoolbox::radians(360) == 2 * std::numbers::pi);
+    assert(smalltoolbox::angle(2 * std::numbers::pi) == 360);
+
+    assert(smalltoolbox::angle(0.0, 0.0,  0.0,  0.0) == 0);
+    assert(smalltoolbox::angle(0.0, 0.0,  1.0,  0.0) == 0);
+    assert(smalltoolbox::angle(0.0, 0.0,  0.0,  1.0) == 90);
+    assert(smalltoolbox::angle(0.0, 0.0, -1.0,  0.0) == 180);
+    assert(smalltoolbox::angle(0.0, 0.0,  0.0, -1.0) == 270);
+    assert(smalltoolbox::angle(0.0, 0.0,  1.0,  1.0) == 45);
+    assert(smalltoolbox::angle(0.0, 0.0, -1.0,  1.0) == 90 + 45);
+    assert(smalltoolbox::angle(0.0, 0.0, -1.0, -1.0) == 180 + 45);
+    assert(smalltoolbox::angle(0.0, 0.0,  1.0, -1.0) == 270 + 45);
+    assert(smalltoolbox::angle(1.0, 1.0,  0.0,  0.0) == 180 + 45);
+
+    assert(smalltoolbox::cos(5, 10,  0) == (5 + 10 * 1));
+    assert(smalltoolbox::sin(5, 10,  0) == (5 + 10 * 0));
+    assert(smalltoolbox::sin(5, 10, 90) == (5 + 10 * 1));
+
+    assert(smalltoolbox::distance(0.0, 0.0, 5.0, 0.0) == 5);
+    assert(smalltoolbox::distance(0.0, 0.0, 0.0, 5.0) == 5);
+    assert(smalltoolbox::distance(0.0, 0.0, 5.0, 5.0) == std::sqrt(50));
+
+    // Point
     Point p1, p2; // (0, 0)
 
     assert(p1.equal(p2));
     assert(p1 == Point(0, 0));
-    assert(p1 + Point(1, 1) == Point(1, 1));
+    assert(p1 + Point(1, 1) == Point(1,  1));
     assert(p1 - Point(1, 1) == Point(-1, -1));
-    assert(p1 * Point(2, 2) == Point(0, 0));
+    assert(p1 * Point(2, 2) == Point(0,  0));
+    assert(Point(5, 5) * Point(0.5, 1 / 2)   == Point(2.5, 0));
+    assert(Point(5, 5) * Point(0.5, 1 / 2.0) == Point(2.5, 2.5));
 
+    p2 = Point(1, 1);
+    p2 *= 3;
+    p2 += 2;
+    p2 -= 4;
+    assert(p2 == Point(1, 1));
+
+    p1 += Point(1, 1);
+    assert(p1 == p2);
+
+    // Output
     view(1);
     view(0.5);
     view(1.5f);
 
     view(p1);
+    view(p2);
     view(p1.XY());
-}
 
+    // Line
+    view(Line(Point(1, 1), Point(2, 2)).points());
+
+    // Polygon
+    Polygon polygon;
+    view(polygon.setup(Point(0, 0), -1,  0,  0)); // Return empty
+    view(polygon.setup(Point(0, 0),  0,  0, -1)); // Return empty
+    view(polygon.setup(Point(1, 1),  0,  0,  0)); // Return empty
+    view(polygon.setup(Point(0, 0),  1,  0,  1)); // Return empty
+    view(polygon.setup(Point(0, 0),  1,  0,  3)); // Triangle
+    view(polygon.setup(Point(0, 0),  1,  0,  4)); // Rectangle
+    view(polygon.setup(Point(0, 0),  1, 45,  4)); // Rectangle
+    view(polygon.setup(Point(0, 0),  2, 90,  4)); // Rectangle
+    view(polygon.setup(Point(0, 0),  1,  0,  5)); // Pentagon
+    view(polygon.points()); // returns the last configuration points
+
+    // Rectangle (1,0)(0,1)(-1,0)(0,-1)
+    polygon.setup(Point(0, 0),  1,  0,  4);
+    auto a = polygon.sideLength();
+    auto b = Point(1, 0).distance(Point(0, 1));
+    view(std::to_string(a) == std::to_string(b) ? "true" : "false"); // true
+    view(a == b ? "true" : "false");                                 // false
+    view(polygon.diagonalLength() == 2.0 ? "true" : "false");        // true
+    view(polygon.points());
+
+    // SVG
+    view("SVG");
+    auto shape = SVG::Shape("Rectangle", RED, GREEN, 1.0,
+                            Polygon().setup(Point(50, 50), 50, 45, 4));
+    auto svg = SVG::svg(100, 100, SVG::polygon(shape), SVG::Metadata());
+    save(svg, "svgOutput.svg");
+    view(svg);
+}
